e7d00c1c8385ed29f7d92b3e6461a61824160d4b
eddygarros <eddygarros@hotmail.com>
2022-03-16 10:21:59 -0600

    first commit

---
+++ ISubscription.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using baffis.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace baffis.DataAccess.Interface
+{
+    public interface ISubscription
+    {
+        IEnumerable<Subscription> List();
+
+        IActionResult Create(Subscription item);
+        IActionResult Read(Subscription item);
+
+        IActionResult Update(Subscription item);
+
+        IActionResult Delete(Subscription item);
+    }
+}
